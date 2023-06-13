b0db2dda0eca100e2c87aa9a661d6beeb43798d2
jmamedina <johnmichaelmedina1@gmail.com>
2023-06-13 17:21:11 +0900

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container-lg" style="margin: 0 auto;">
+    <div class="row mt-5">
+
+
+        <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3"> 
+            <div class="card" style="width: 18rem;">
+                <div class="card-title"> Title </div>
+                <div class="card-text"> content</div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3"> 
+            <div class="card" style="width: 18rem;">
+                <div class="card-title"> Title </div>
+                <div class="card-text"> content</div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3"> 
+            <div class="card" style="width: 18rem;">
+                <div class="card-title"> Title </div>
+                <div class="card-text"> content</div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3"> 
+            <div class="card" style="width: 18rem;">
+                <div class="card-title"> Title </div>
+                <div class="card-text"> content</div>
+            </div>
+        </div>
+
+        
+    </div>
+</div>
+
+@endsection
